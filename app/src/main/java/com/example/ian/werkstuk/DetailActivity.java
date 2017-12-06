44345b15cf7bb41c8b6a7dd7d2f9752b8fcec03b
iankerkhove <ian.kerkhove@student.ehb.be>
2017-12-06 12:11:34 +0100

    detailsactivity
    detailsactivity voor movie is af

--- app/src/main/java/com/example/ian/werkstuk/DetailActivity.java
+++ app/src/main/java/com/example/ian/werkstuk/DetailActivity.java
@@ -1,6 +1,9 @@
 package com.example.ian.werkstuk;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -17,10 +21,15 @@ import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 
@@ -29,6 +38,17 @@ public class DetailActivity extends AppCompatActivity {
     private String BASE_URL = "http://api.themoviedb.org/3/movie/";
     private String META = "?api_key=1da7f7f08b98f2fb0be745269a36728b&language=en-US";
     TextView titel;
+    TextView tagline;
+    TextView release;
+    TextView orilanguage;
+    TextView spolanguage;
+    TextView genre;
+    TextView statu;
+    TextView productionCountry;
+    TextView votesAverage;
+    TextView overview;
+    ImageView image;
+    private Bitmap bmp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -96,10 +116,87 @@ public class DetailActivity extends AppCompatActivity {
                 JSONObject object = null;
                 try {
                     object = new JSONObject(response);
-                    TextView tagline = (TextView) findViewById(R.id.tagline);
-                    TextView releasedate = (TextView) findViewById(R.id.releasedate);
-                    releasedate.setText(object.getString("release_date"));
-                    tagline.setText(object.toString());
+                    tagline = (TextView) findViewById(R.id.tagline);
+                    release = (TextView) findViewById(R.id.releaseDate);
+                    orilanguage = (TextView) findViewById(R.id.originalLanguage);
+                    spolanguage = (TextView) findViewById(R.id.spokenLanguage);
+                    genre = (TextView) findViewById(R.id.genres);
+                    statu = (TextView) findViewById(R.id.status);
+                    productionCountry = (TextView) findViewById(R.id.prodCountry);
+                    votesAverage = (TextView) findViewById(R.id.voteAverage);
+                    overview = (TextView) findViewById(R.id.overview);
+                    image = (ImageView) findViewById(R.id.headImage);
+                    final String imagePath = object.get("poster_path").toString();
+
+
+                    //https://stackoverflow.com/questions/24535924/how-to-get-image-from-url-website-in-imageview-in-android
+                    new AsyncTask<Void, Void, Void>() {
+                        @Override
+                        protected Void doInBackground(Void... params) {
+                            try {
+                                InputStream in = new URL("https://image.tmdb.org/t/p/w300/"+ imagePath).openStream();
+                                bmp = BitmapFactory.decodeStream(in);
+                            } catch (Exception e) {
+                                // log error
+                            }
+                            return null;
+                        }
+
+                        @Override
+                        protected void onPostExecute(Void result) {
+                            if (bmp != null)
+                                image.setImageBitmap(bmp);
+                        }
+
+                    }.execute();
+                    //image.setScaleType(ImageView.ScaleType.FIT_XY);
+
+                    release.setText(object.get("release_date").toString());
+                    tagline.setText(object.get("tagline").toString());
+                    orilanguage.setText(object.get("original_language").toString());
+                    String language ="";
+                    for(int i=0;i< object.getJSONArray("spoken_languages").length();i++){
+                        JSONArray a = new JSONArray();
+                        a = object.getJSONArray("spoken_languages");
+                        String value = a.getJSONObject(i).get("iso_639_1").toString();
+                        if(a.length()>1){
+                            language += value +", ";
+                        }
+                       else{
+                            language += value;
+                        }
+                    }
+                    String genres ="";
+                    spolanguage.setText(language);
+                    for(int i=0;i< object.getJSONArray("genres").length();i++){
+                        JSONArray a = new JSONArray();
+                        a = object.getJSONArray("genres");
+                        String value = a.getJSONObject(i).get("name").toString();
+                        if(a.length()>1){
+                            genres += value +", ";
+                        }
+                        else{
+                            genres += value;
+                        }
+                    }
+                    genre.setText(genres);
+                    statu.setText(object.get("status").toString());
+                    String production="";
+                    for(int i=0;i< object.getJSONArray("production_countries").length();i++){
+                        JSONArray a = new JSONArray();
+                        a = object.getJSONArray("production_countries");
+                        String value = a.getJSONObject(i).get("iso_3166_1").toString();
+                        if(a.length()>1){
+                            production += value +", ";
+                        }
+                        else{
+                            production += value;
+                        }
+                    }
+                    productionCountry.setText(production);
+                    votesAverage.setText(object.get("vote_average").toString());
+                    overview.setText(object.get("overview").toString());
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
