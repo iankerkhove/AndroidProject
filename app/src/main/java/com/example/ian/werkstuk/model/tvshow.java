3d2b3960bf06f1e0dff1e5f0889c5fc24e562642
iankerkhove <ian.kerkhove@student.ehb.be>
2017-12-27 10:15:09 +0100

    model
    movie en tvshow model toegevoegd

--- app/src/main/java/com/example/ian/werkstuk/model/tvshow.java
+++ app/src/main/java/com/example/ian/werkstuk/model/tvshow.java
@@ -1,18 +1,16 @@
 package com.example.ian.werkstuk.model;
 
-/**
- * Created by ian on 13/12/2017.
- */
-
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.media.Image;
 
-import java.io.Serializable;
+/**
+ * Created by ian on 27/12/2017.
+ */
 
 @Entity
-public class movie implements Serializable {
+public class tvshow {
     @PrimaryKey()
     private int id;
 
@@ -21,9 +19,6 @@ public class movie implements Serializable {
 
     @ColumnInfo(name = "genre")
     private String genre;
-
-    @ColumnInfo(name = "tagline")
-    private String tagline;
 
     @ColumnInfo(name = "releasedate")
     private String releasedate;
@@ -34,11 +29,11 @@ public class movie implements Serializable {
     @ColumnInfo(name = "originalLanguage")
     private String oriLanguage;
 
-    @ColumnInfo(name = "spokenLanguage")
-    private String spoLanguage;
+    @ColumnInfo(name = "productionCompany")
+    private String prodCompany;
 
-    @ColumnInfo(name = "productionCountry")
-    private String prodCountry;
+    @ColumnInfo(name = "numberOfSeasons")
+    private String numberOfSeasons;
 
     @ColumnInfo(name = "voteAverage")
     private String voteAverage;
@@ -49,20 +44,28 @@ public class movie implements Serializable {
     @ColumnInfo(name = "image")
     private Image image;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getGenre() {
         return genre;
     }
 
     public void setGenre(String genre) {
         this.genre = genre;
-    }
-
-    public String getTagline() {
-        return tagline;
-    }
-
-    public void setTagline(String tagline) {
-        this.tagline = tagline;
     }
 
     public String getReleasedate() {
@@ -89,20 +92,20 @@ public class movie implements Serializable {
         this.oriLanguage = oriLanguage;
     }
 
-    public String getSpoLanguage() {
-        return spoLanguage;
+    public String getProdCompany() {
+        return prodCompany;
     }
 
-    public void setSpoLanguage(String spoLanguage) {
-        this.spoLanguage = spoLanguage;
+    public void setProdCompany(String prodCompany) {
+        this.prodCompany = prodCompany;
     }
 
-    public String getProdCountry() {
-        return prodCountry;
+    public String getNumberOfSeasons() {
+        return numberOfSeasons;
     }
 
-    public void setProdCountry(String prodCountry) {
-        this.prodCountry = prodCountry;
+    public void setNumberOfSeasons(String numberOfSeasons) {
+        this.numberOfSeasons = numberOfSeasons;
     }
 
     public String getVoteAverage() {
@@ -128,20 +131,5 @@ public class movie implements Serializable {
     public void setImage(Image image) {
         this.image = image;
     }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 }
+
